4bf5fa454ade89385091b2ea486a43dbd0aa5470
Regis Philibert <login@regisphilibert.com>
2018-08-22 15:28:53 -0400

    Finalization of Algolia InstantSearch implementation - Styling - Master rebase

--- themes/rougon/assets/js/main.js
+++ themes/rougon/assets/js/main.js
@@ -1,36 +1,3 @@
-
-
-// passing in `null` for the `options` arguments will result in the default
-// options being used
-
-$.getJSON('./index.json', {param1: 'value1'}, function(json, textStatus) {
-	var items = new Bloodhound({
-		datumTokenizer: Bloodhound.tokenizers.obj.whitespace("title", "type"),
-		queryTokenizer: Bloodhound.tokenizers.whitespace,
-		// url points to a json file that contains an array of country names, see
-		// https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-		local:json
-	});
-	items.initialize();
-
-	$('.main-autocomplete').typeahead(null, 
-        {
-            name: 'items',
-            source: items,
-            display:'title',
-            templates: {
-                suggestion: function (data) {
-                    type = data.type == 'roman' ? ' <small>(' + data.type + ')</small>' : ''
-        			return '<div class="item"><span>' + data.title + '</span>'+ type + '</div></div>';
-    			}
-            }
-        },
-	);
-});
-/*var jsonData = [
-{"type":"novel","title":"Nana"},
-{"type":"character", "title": "Nana Femme"},
-];*/
 $(document).ready(function(){
    $('.main-autocomplete').bind('typeahead:selected', function(obj, output, name) {
         var params;
